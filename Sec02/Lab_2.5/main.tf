bcd38f2c9387e15f3b07f44c8965760f889d5f60
Mor Codou GUEYE <morcodou@gmail.com>
2021-05-14 15:08:22 -0400

    format sec02-5

--- Sec02/Lab_2.5/main.tf
+++ Sec02/Lab_2.5/main.tf
@@ -9,19 +9,19 @@ resource "azurerm_resource_group" "resourcegroup" {
 }
 
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "gmcterraformazkv"
-  location                    = azurerm_resource_group.resourcegroup.location
-  resource_group_name         = azurerm_resource_group.resourcegroup.name
-  tenant_id                   = TENANT_ID
-  sku_name                    = "standard"
+  name                = "gmcterraformazkv"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  tenant_id           = TENANT_ID
+  sku_name            = "standard"
 }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                      = "gmcterraformazsac"
-  resource_group_name       = azurerm_resource_group.resourcegroup.name
-  location                  = azurerm_resource_group.resourcegroup.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
+  name                     = "gmcterraformazsac"
+  resource_group_name      = azurerm_resource_group.resourcegroup.name
+  location                 = azurerm_resource_group.resourcegroup.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 # # Please use terraform v12.29 to start with for all labs, I will use terraform v13 and v14 from lab 7.5 onwards
