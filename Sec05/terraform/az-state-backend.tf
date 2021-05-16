d59cc8a80ca766ab621684aedd9a8eb0a432f557
Mor Codou GUEYE <morcodou@gmail.com>
2021-05-16 00:05:54 -0400

    add interpolation and backend cmd

--- Sec05/terraform/az-state-backend.tf
+++ Sec05/terraform/az-state-backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "gmc-storage-rg"
-    storage_account_name = "gmctfazsac"
-    container_name       = "tfstate"
-    key                  = "az-backend.tfsate"
-  }
+  backend "azurerm" {}
 }
