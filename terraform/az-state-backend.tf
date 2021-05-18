96ce3bb0f6ef40dfebd0fde0e42513b226a356f1
Mor Codou GUEYE <morcodou@gmail.com>
2021-05-18 01:56:40 -0400

    add service principal

--- terraform/az-state-backend.tf
+++ terraform/az-state-backend.tf
@@ -16,3 +16,8 @@ terraform {
   #   key                  = "workspace.tfsate"
   # }
 }
+
+# export ARM_CLIENT_ID="AZURE_CLIENT_ID"
+# export ARM_CLIENT_SECRET="AZURE_CLIENT_SECRET"
+# export ARM_TENANT_ID="AZURE_TENANT_ID"
+# export ARM_SUBSCRIPTION_ID="AZURE_SUBSCRIPTION_ID"
