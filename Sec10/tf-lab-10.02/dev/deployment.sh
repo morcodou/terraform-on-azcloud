0848ac95c2a675186f5f52689cd9858b4ed67ad8
Mor Codou GUEYE <morcodou@gmail.com>
2021-05-18 23:40:14 -0400

    update deployment bash

--- Sec10/tf-lab-10.02/dev/deployment.sh
+++ Sec10/tf-lab-10.02/dev/deployment.sh
@@ -2,7 +2,7 @@ export ARM_CLIENT_ID=$(SP_CLIENT_ID)
 export ARM_CLIENT_SECRET=$(SP_CLIENT_SECRET)
 export ARM_SUBSCRIPTION_ID=$(SP_SUBSCRIPTION_ID)
 export ARM_TENANT_ID=$(SP_TENANT_ID)
-terraform init -backend-config=backend.tfvars
+terraform init -backend-config backend.tfvars
 terraform plan
 terraform apply -auto-approve
 unset ARM_CLIENT_ID
